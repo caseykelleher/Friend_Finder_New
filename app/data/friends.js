e69061a76f32e597891798df1f2e362e941e54ab
Casey K <caseykelleher@icloud.com>
2020-01-18 15:04:36 -0500

    added old files

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Kevin',
+		photo: 'https://images.app.goo.gl/Q2LmrVC37tFfRmX78',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lila',
+		photo: 'https://images.app.goo.gl/ZcXc4HW8q4bxczNK6',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Leah',
+		photo: 'https://images.app.goo.gl/HNkvXFbdCdUvgiiP9',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Frank',
+		photo: 'https://images.app.goo.gl/h36W7T6r31xNcifM7',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Chris',
+		photo: 'https://images.app.goo.gl/9XqSGe7fK3P65RQn6',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Joe',
+		photo: 'https://images.app.goo.gl/Bohv2S9qZUMufgEb6',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Alan',
+		photo: 'https://images.app.goo.gl/YufmfWtLakZdbkdf6',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Karen',
+		photo: 'https://images.app.goo.gl/gDZppKZPuxt1udfi6',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Charles',
+		photo: 'https://images.app.goo.gl/VRV27jrvXdHUJ1yt7',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Elizabeth',
+		photo: 'https://images.app.goo.gl/v8LKX3k6eWrMwitw9',
+		scores: [
+			'1',
+			'1',
+			'3',
+			'2',
+			'1',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
